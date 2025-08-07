fcac305af11dc2bf3442fb64ed6755ce4612e239
KK <krzyskow@gmail.com>
2025-08-07 18:39:36 +0200

    init

--- src/main/java/kk/pivotcsv/Csv.java
+++ src/main/java/kk/pivotcsv/Csv.java
@@ -1,4 +1,18 @@
 package kk.pivotcsv;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
 public class Csv {
+
+    private final List<String> headers = new ArrayList<>();
+    private final List<Record> records = new ArrayList<>();
+
+
+
+
 }
---
+++ src/main/java/kk/pivotcsv/Record.java
@@ -0,0 +1,6 @@
+package kk.pivotcsv;
+
+import java.util.HashMap;
+
+public class Record extends HashMap<String, String> {
+}
